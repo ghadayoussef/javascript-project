4f7b190b490a2a0717618f108378eea53be0c721
Islam44 <islamemam95@yahoo.com>
2019-12-22 13:53:57 +0200

    add cache to page

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 let productArray=[]
 function getProducts() {
-    fetch("https://gist.githubusercontent.com/a7med-hussien/7fc3e1cba6abf92460d69c0437ce8460/raw/da46abcedf99a3d2bef93a322641926ff60db3c3/products.json")
+    fetch("https://gist.githubusercontent.com/a7med-hussien/7fc3e1cba6abf92460d69c0437ce8460/raw/da46abcedf99a3d2bef93a322641926ff60db3c3/products.json",
+        {cache:"force-cache"})
         .then((resp) => resp.json())
         .then(function(data) {
             data.ProductCollection.forEach((product,index)=>{
