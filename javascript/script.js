bc2a4f5b0a03dbd8f6147481cf3ecc5988a4522e
Islam44 <islamemam95@yahoo.com>
2019-12-22 18:50:08 +0200

    add cache,method header,mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,12 @@
 let productArray=[]
 function getProducts() {
-    fetch("https://gist.githubusercontent.com/a7med-hussien/7fc3e1cba6abf92460d69c0437ce8460/raw/da46abcedf99a3d2bef93a322641926ff60db3c3/products.json",
-        {cache:"force-cache"})
+    fetch("https://gist.githubusercontent.com/a7med-hussien/7fc3e1cba6abf92460d69c0437ce8460/raw/da46abcedf99a3d2bef93a322641926ff60db3c3/products.json"
+        ,{
+           cache:"force-cache",
+            method: "GET",
+            mode: "cors"
+        }
+        )
         .then((resp) => resp.json())
         .then(function(data) {
             data.ProductCollection.forEach((product,index)=>{
